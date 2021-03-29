6cbca3c4efac4bee5222605cc00030146665d7e7
Andre Göhring <a-goehring@web.de>
2021-03-29 16:11:48 +0200

    Implementing some recursion for better functional style Reducing the size of duplicate code

--- src/main/scala/de/htwg/se/ticTacToe3D/model/FourDGridsStateStrategy.scala
+++ src/main/scala/de/htwg/se/ticTacToe3D/model/FourDGridsStateStrategy.scala
@@ -6,45 +6,54 @@ case class FourDGridsStateStrategy(verticalColumn: Array[Array[Int]] =
                               List.fill(4)(Array.tabulate(4, 4) { (row, col) => 0}))
   extends WinStateStrategyTemplate {
 
-  override def checkRow(row: Int, grid: Int): Boolean = {
-    var rowLength = 0
-    var reverseRowLength = 0
-    for (i <- this.diagOfAllGrids.indices) {
-      rowLength += diagOfAllGrids(i)(row)(i)
-      reverseRowLength+= diagOfAllGrids(i)(row)(3 - i)
+  def checkCell(dir: String)(row: Int, column: Int, i : Int, j: Int, k : Int): Boolean = {
+    var length = j
+    var reverseLength = k
+    if (numberToWin == length || numberToWin == reverseLength || this.verticalColumn(row)(column) == numberToWin) return true
+    if (i == this.diagOfAllGrids.length) return false
+
+    length = setGridScore(dir)(row, column, i, length, reverseLength)._1
+    reverseLength = setGridScore(dir)(row, column, i , length, reverseLength)._2
+
+    checkCell(dir)(row, column, i + 1, length, reverseLength)
+  }
+
+  def setGridScore(dir: String)(row: Int, column: Int, i : Int ,  j: Int, k : Int): (Int, Int) = {
+    var length = j
+    var reverseLength = k
+    dir match {
+      case "row" =>
+        length += diagOfAllGrids(i)(row)(i)
+        reverseLength += diagOfAllGrids(i)(row)(3 - i)
+      case "col" =>
+        length += diagOfAllGrids(i)(i)(column)
+        reverseLength += diagOfAllGrids(i)(3 - i)(column)
+      case "diagonal" =>
+        length += diagOfAllGrids(i)(i)(i)
+        reverseLength += diagOfAllGrids(i)(3 - i)(3 - i)
+      case "diagonalReverse" =>
+        length += diagOfAllGrids(i)(3 - i)(i)
+        reverseLength += diagOfAllGrids(i)(i)(3 - i)
+      case _ => println("ERROR")
     }
-    rowLength == numberToWin || reverseRowLength == numberToWin
+    (length, reverseLength)
+  }
+
+  override def checkRow(row: Int, grid: Int): Boolean = {
+    this.checkCell("row")(row, 0, 0, 0, 0)
   }
 
   override def checkColumn(row: Int, column: Int, grid: Int): Boolean = {
-    var columnLength = 0
-    var reverseColumnLength = 0
-    for (i <- this.diagOfAllGrids.indices) {
-      columnLength += diagOfAllGrids(i)(i)(column)
-      reverseColumnLength += diagOfAllGrids(i)(3 - i)(column)
-    }
     this.verticalColumn(row)(column) += 1
-    this.verticalColumn(row)(column) == numberToWin || columnLength == numberToWin || reverseColumnLength == numberToWin
+    this.checkCell("col")(row, column, 0, 0, 0)
   }
 
   def checkDiagOfAllGridsTogetherColumnEqualRow: Boolean = {
-    var rowLength = 0
-    var reverseRowLength = 0
-    for (i <- this.diagOfAllGrids.indices) {
-      rowLength += diagOfAllGrids(i)(i)(i)
-      reverseRowLength+= diagOfAllGrids(i)(3 - i)(3 - i)
-    }
-    rowLength == numberToWin || reverseRowLength == numberToWin
+    this.checkCell("diagonal")(0, 0, 0, 0, 0)
   }
 
   def checkDiagOfAllGridsTogetherInOtherSideOfDiagOneGrid: Boolean = {
-    var rowLength = 0
-    var reverseRowLength = 0
-    for (i <- this.diagOfAllGrids.indices) {
-      rowLength += diagOfAllGrids(i)(3 - i)(i)
-      reverseRowLength+= diagOfAllGrids(i)(i)(3 - i)
-    }
-    rowLength == numberToWin || reverseRowLength == numberToWin
+    this.checkCell("diagonalReverse")(0, 0, 0, 0, 0)
   }
 
   override def checkDiagonal(row: Int, column: Int, grid: Int): Boolean = {
--- src/main/scala/de/htwg/se/ticTacToe3D/model/OneDGridsStateStrategy.scala
+++ src/main/scala/de/htwg/se/ticTacToe3D/model/OneDGridsStateStrategy.scala
@@ -5,31 +5,27 @@ case class OneDGridsStateStrategy(rowGridScore: List[Array[Int]] = List.fill(4)(
                              diagGridScore: List[Array[Int]] = List.fill(4)(Array(0, 0)))
   extends WinStateStrategyTemplate {
 
-  override def checkRow(row: Int, grid: Int): Boolean = {
-    this.rowGridScore(grid)(row) += 1
-    this.rowGridScore(grid)(row) == numberToWin
+  def setGridScore(score: List[Array[Int]])(cell: Int, grid: Int): Boolean = {
+    score(grid)(cell) += 1
+    score(grid)(cell) == numberToWin
   }
 
-  override def checkColumn(row: Int, column: Int, grid: Int): Boolean = {
-    this.colsGridScore(grid)(column) += 1
-    this.colsGridScore(grid)(column) == numberToWin
-  }
+  override def checkRow(row: Int, grid: Int): Boolean = this.setGridScore(rowGridScore)(row, grid)
 
-  def checkCellInOtherSideOfDiagOneGrid(row: Int, column: Int, grid: Int): Boolean = {
-    for (i <- 0 to numberToWin) {
-      if (row == i && column == (numberToWin - 1 - i)) {
-        this.diagGridScore(grid)(1) += 1
-        return this.diagGridScore(grid)(1) == numberToWin
-      }
+  override def checkColumn(row: Int, column: Int, grid: Int): Boolean = this.setGridScore(colsGridScore)(column, grid)
+
+  def checkCellInOtherSideOfDiagOneGrid(row: Int, column: Int, grid: Int, i: Int): Boolean = {
+    if (i == numberToWin) return false
+    if (row == i && column == (numberToWin - 1 - i)) {
+      return this.setGridScore(diagGridScore)(1, grid)
     }
-    false
+    checkCellInOtherSideOfDiagOneGrid(row, column, grid, i + 1)
   }
 
   override def checkDiagonal(row: Int, column: Int, grid: Int): Boolean = {
     if (row == column) {
-      this.diagGridScore(grid)(0) += 1
-      return this.diagGridScore(grid)(0) == numberToWin
+      return this.setGridScore(diagGridScore)(0, grid)
     }
-    checkCellInOtherSideOfDiagOneGrid(row, column, grid)
+    checkCellInOtherSideOfDiagOneGrid(row, column, grid, 0)
   }
 }
